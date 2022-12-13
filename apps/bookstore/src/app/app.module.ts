35d78f963e6e68a52a6824dc569803f19c501b31
Eliran Eliassy <eliran.eliassy@gmail.com>
2022-12-13 13:29:54 +0200

    add material

--- apps/bookstore/src/app/app.module.ts
+++ apps/bookstore/src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { BookComponent } from './book/book.component';
 import { DiscountPipe } from './discount.pipe';
 import { ChangeColorDirective } from './change-color.directive';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, BookComponent, DiscountPipe, ChangeColorDirective],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
